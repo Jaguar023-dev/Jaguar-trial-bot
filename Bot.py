c5fc0c5a4588f9f65ab24b2533c9eb2d496c86c0
Jaguar023-dev <derrickithicia8@gmail.com>
2025-09-09 18:32:44 +0300

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -11,3 +11,15 @@ def reply(msg):
         msg.reply('Hello, how are you?')
     elif msg.body.lower() == 'hi':
         msg.reply('Hi, how are you?')
+@client.on_message()
+def reply(msg):
+    if msg.body.lower() == 'hello':
+        msg.reply('Hello, how are you?')
+    elif msg.body.lower() == 'hi':
+        msg.reply('Hi, how are you?')
+    elif msg.body.lower() == 'image':
+        msg.reply('Here is an image', media='https://picsum.photos/200/300')
+    elif msg.body.lower() == 'video':
+        msg.reply('Here is a video', media='http://techslides.com/demos/sample-videos/small.mp4')
+    else:
+        msg.reply('Hello '+msg.sender.push_name+', how can I help you?')
