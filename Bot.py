d58d5cf2675db9c96c91a58b036104d6f9d25922
Jaguar023-dev <derrickithicia8@gmail.com>
2025-09-09 18:31:27 +0300

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -5,3 +5,9 @@ from pywhatsapp import Client
 from datetime import datetime
 client = Client()
 client.login()
+@client.on_message()
+def reply(msg):
+    if msg.body.lower() == 'hello':
+        msg.reply('Hello, how are you?')
+    elif msg.body.lower() == 'hi':
+        msg.reply('Hi, how are you?')
